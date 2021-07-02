e66402f339ff82818dc3c33f3bb9f8d444ef4f50
Mohit Singh <mohitsingh.shiv@gmail.com>
2021-07-02 13:19:40 +0530

    Changes for #53

---
+++ Source/Service/Glasswall.EBS.Rebuild/ConfigLoaders/EbsConfigLoader.cs
@@ -0,0 +1,15 @@
+﻿using Glasswall.EBS.Rebuild.Configuration;
+
+namespace Glasswall.EBS.Rebuild.ConfigLoaders
+{
+    public static class EbsConfigLoader
+    {
+        public static IEbsConfiguration SetDefaults(IEbsConfiguration configuration)
+        {
+            configuration.RETRY_COUNT = Constants.RetryCount;
+            configuration.CRONJOB_PERIOD = Constants.CronjobPeriod;
+            configuration.FORLDERS_PATH = Constants.ForldersPath;
+            return configuration;
+        }
+    }
+}
